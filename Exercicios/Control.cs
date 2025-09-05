f1a130fb54ce579b93dd98c29c211673b6bc871e
STEFANY CARLOS BARBOSA <stefany.cbarbosa@senacsp.edu.br>
2025-09-05 12:03:39 -0300

    terceiro commit

--- Exercicios/Control.cs
+++ Exercicios/Control.cs
@@ -28,13 +28,34 @@ namespace Exercicios
                               "\n0. Sair" +
                               "\n1. Exercício 1" +
                               "\n2. Exercício 2" +
-                              "\n3. Exercício 3" + //novo
-                              "\n5. Exercício 5" + //novo
-                              "\n6. Exercício 6" + //novo
-                              "\n7. Exercício 7" + //novo
-                              "\n8. Exercício 8" +
-                              "\n9. Exercício 9" +
-                              "\n16. Exercício 16 ");
+                              "\n3. Exercício 3" + 
+                              "\n4. Exercício 4 " +
+                              "\n5. Exercício 5" +
+                              "\n6. Exercício 6" + 
+                              "\n7. Exercício 7" + 
+                              "\n10. Exercício 10" + 
+                              "\n11. Exercício 11" +
+                              "\n12. Exercício 12" +
+                              "\n13. Exercício 13" +
+                              "\n14. Exercício 14" +
+                              "\n15. Exercício 15" + //nao
+                              "\n16. Exercício 16" +
+                              "\n17. Exercício 17" + 
+                              "\n18. Exercício 18" + 
+                              "\n19. Exercício 19" +
+                              "\n20. Exercício 20" +
+                              "\n21. Exercício 21" +
+                              "\n22. Exercício 22" +
+                              "\n23. Exercício 23" +
+                              "\n24. Exercício 24" +
+                              "\n25. Exercício 25" +
+                              "\n26. Exercício 26 " +
+                              "\n26. Exercício 27 " +
+                              "\n26. Exercício 28" +
+                              "\n29. Exercício 29" +
+                              "\n26. Exercício 30 " +
+                              "\n26. Exercício 31" +
+                              " \n29. Exercício 32 ");
             ModificarOpcao= Convert.ToInt32(Console.ReadLine());
         }//fim do mostrar menu 
 
@@ -67,7 +88,13 @@ namespace Exercicios
                         Console.WriteLine("Exercício 3");
                         Console.WriteLine("Informe o valor em Dólar");
                         double dolar = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine(modelo.ConverterParaVinte(dolar));                      
+                        Console.WriteLine($"O valor em reais é de : {modelo.ConverterDolar(dolar)}");                      
+                        break;
+                    case 4:
+                        Console.WriteLine("Exercício 4");
+                        Console.WriteLine("Informe o valor da base");
+                        double basee = Convert.ToDouble(Console.ReadLine());  
+                        Console.WriteLine("O valor do Perimetro é " + modelo.CalcularPerimetro(basee));
                         break;
                     case 5:
                         Console.WriteLine("Exercício 05");
@@ -136,7 +163,45 @@ namespace Exercicios
                             Console.WriteLine($"O volume do cilindro é: {modelo.VolumeCilindro(raio, altura)}");
                         }
                         break;
+                    case 10:
+                        Console.WriteLine("Exercício 10");
+                        Console.WriteLine("Digite um número");
+                        double absoluto =Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O valor absoluto é : {modelo.ValorAbsoluto(absoluto)}");
+                        break;
+                    case 11:
+                        Console.WriteLine("Exercício 11");
+                        Console.WriteLine("Informe o valor em Quilometros");
+                        double metros = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O valor em metros é {modelo.ConverterKM(metros)}");
+                        break;
+                    case 12:
+                        Console.WriteLine("Exercício 12");
+                        Console.WriteLine("Informe um número");
+                        double potencia = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"A quinta potência é {modelo.Calcular5(potencia)}");
+                        break;
+                    case 13:
+                        Console.WriteLine("Exercício 13");
+                        Console.WriteLine("Informe a Distancia");
+                        double distancia = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe a Velocidade");
+                        double velocidade = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"o tempo sera de : {modelo.CalcularTempo(distancia, velocidade)}");
+                        break;
 
+                        case 14:
+                        Console.WriteLine("Exercício 14");
+                        Console.WriteLine("Informe os kilometros percorridos");
+                        double km = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe os litros usados");
+                        double litros = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O consumo de combustível foi de {modelo.CalcularCombustivel(km, litros)}");
+                            break;
+                    case 15:
+                        Console.WriteLine("Exercício 15");
+                        Console.WriteLine("Informe um Número");
+                        break;
                     case 16:
                         Console.WriteLine("Exercício 16");
                         Console.WriteLine("Informe o seu peso:");
@@ -148,11 +213,110 @@ namespace Exercicios
                         Console.WriteLine(modelo.CalcularIMC(peso, altura));
                         break;
 
+                    case 17:
+                        Console.WriteLine("Exercicío 17");
+                        Console.WriteLine("Informe seu salário mensal");
+                        double mensal = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O salário anual é de {modelo.CalcularSalario(mensal)}");
+                        break;
+
+                    case 18:
+                        Console.WriteLine("Exercício 18");
+                        Console.WriteLine("Informe o número");
+                        double log = Convert.ToDouble(Console.ReadLine());                 
+                            Console.WriteLine($"O logaritmo é {modelo.Logaritmo(log)}");
+                        break;
+
+                    case 19:
+                        Console.WriteLine("Exercicío 19");
+                        Console.WriteLine("Informe o valor do Cateto");
+                        double catetoO = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe o valor da Hipotenusa");
+                        double hipotenusa = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O valor do Seno é {modelo.CalcularSeno(catetoO, hipotenusa)}");                     
+                        break;
+                    case 20:
+                        Console.WriteLine("Exercício 20");
+                        Console.WriteLine("Informe o valor do Cateto Adjacente");
+                        double adjacente = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe o valor da Hipotensa");
+                        double hipo = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O valor do Cosseno é {modelo.CalcularCosseno(adjacente,hipo)}");
+                        break;
+
+                    case 21:
+                        Console.WriteLine("Exercício 21");
+                        Console.WriteLine("Informe a Base Maior");
+                        double baseM= Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe a Base Menor");
+                        double baseP = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe a altura ");
+                        double alt = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"A area do Triangulo Trapezio é de : {modelo.AreaTrapezio(baseM, baseP, alt)}");
+                    break;
+
+                    case 22:
+                       Console.WriteLine("Exercicío 22");
+                        Console.WriteLine("Informe os dias");
+                        double dias = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O valor em segundos é de : {modelo.ConverterDias(dias)}");  
+                        break;
+                    
+                    case 23:
+                        Console.WriteLine("Exercicío 23");
+                        Console.WriteLine("Digite um número");
+                        int numero = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"A soma do número é de : {modelo.SomaDigitos(numero)}");
+                        break;
+                    
+                    case 24:
+                        Console.WriteLine("Exercicío 24");
+                        Console.WriteLine("Informe um número");
+                        double inverso = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    case 25:
+                       Console.WriteLine("Exercicio 25");
+                        Console.WriteLine("Informe o valor do primeiro cateto");
+                        double cate1 =  Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe o valor do segundo cateto");
+                        double cate2 =   Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O valor da Hipotenusa é de  : {modelo.CalcularHipotenusa(cate1,cate2)}");
+                        break;
+
+                    case 26:
+                        Console.WriteLine("Exercicío 26");
+                        Console.WriteLine("Informe o valor em Decimal");
+                       int decim= Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Valor em Binário{modelo.Inverter(modelo.ConverterBinario(decim))}");
+                        break;
+
+                    case 27:
+                        Console.WriteLine("Exercício 27");
+                        Console.WriteLine("informe um numero");
+                        decim = Convert.ToInt32(Console.ReadLine());
+                        //OPERAÇÃO
+                        Console.WriteLine($"Valor em Hexadecimal: {modelo.Hexadecimal(decim)}");
+                        break;
+                        //28
+                    case 29:
+                        Console.WriteLine("Exercicío 29");
+                        Console.WriteLine("Informe o montante( terá aplicação de 15% em juros)");
+                        double valor= Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O montante final é de : {modelo.CalcularJuros(valor)}");
+                        break;
+                        //30
+                        //31
+                    case 32:                    
+                        Console.WriteLine("Exercicío 32");
+                        Console.WriteLine("Informe um número");
+                        double porcento = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"O valor em porcentagem é de : {modelo.CalcularPorcentagem(porcento)}");
+                        break;
                     default:
                         Console.WriteLine("Opção escolhida não é válida!");
                         break;
 
-
+                       
 
 
 
--- Exercicios/Exercicio.cs
+++ Exercicios/Exercicio.cs
@@ -41,19 +41,23 @@ namespace Exercicios
         {
             return num / 2;
         }// fim do metodo
-
-        
+         //3.
         public double ConverterDolar(double dolar)
         {
             if (dolar < 0)
             {
                 return -1;
             }
-            else 
+            else
             {
                 return dolar * 5.16;
             }
         }// fim do metodo
+        //4.Calcular Perimetro
+        public double CalcularPerimetro(double basee)
+        {
+            return basee * basee;
+        }//fim do método
         //5.Converta um valor em horas para minutos 
         public double HorasParaMinutos(double horas)
         {
@@ -66,10 +70,10 @@ namespace Exercicios
                 return horas * 60;
             }
         }//fim do método
-       //6.
-       public double ConverterPorcentagem(double porcentagem)
+         //6.
+        public double ConverterPorcentagem(double porcentagem)
         {
-            return porcentagem *0.10;
+            return porcentagem * 0.10;
         }//fim do método
         //7. 
         public double ConverterParaVinte(double vintecinco)
@@ -111,6 +115,68 @@ namespace Exercicios
 
         }// fim do método
 
+        //10.
+        public double ValorAbsoluto(double absoluto)
+        {
+            if (absoluto >= 0)
+            {
+                return absoluto;
+            }
+            else
+            {
+                return -absoluto;
+            }
+        }//fim do método 
+        //.11
+        public double ConverterKM(double metros)
+        {
+            if (metros <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return metros * 1000;
+            }
+        }//fim do método
+        //12.
+        public double Calcular5(double potencia)
+        {
+            if (potencia <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return Math.Pow(potencia, 5);
+            }
+
+        }//fim do metodo oooooo gloria deu certo kkkkk
+        //13.
+        public double CalcularTempo(double distancia, double velocidade)
+        {
+            if (distancia <= 0 && velocidade <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return distancia / velocidade;
+            }
+        }// fim do metodo
+        //14.
+        public double CalcularCombustivel(double km, double litros)
+        {
+            if (km <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return km / litros;
+            }
+        }//fim do método
+  
         //16. Leia a altura e peso de uma pessoa e calcule o IMC 
         // SE IMC <18.5 - Abaixo do peso
         // SE IMC >=18.5 E IMC < 24.9- peso normal 
@@ -149,16 +215,214 @@ namespace Exercicios
                 else
                 {
                     resultado += "Obesidade Grau III";
-                }// FIM DO IF RESULTADO
-            }// FIM DO IF
+                }
+            }// fim do if
             else
             {
                 resultado = "Impossivel calcular IMC com peso e/ou altura negativos";
             }
             return resultado;
-        }//fim do IMC
+        }//fim do metodo IMC
+
+        //17.
+        public double CalcularSalario(double mensal)
+        {
+            return mensal * 12;
+        }// fim do método
+
+        //18. Leia um número e exiba seu logaritmo natural
+        public double Logaritmo(double log)
+        {
+            if (log <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return Math.Log10(log);
+            }
+        }//fim do método
+
+         //19.
+        public double CalcularSeno(double catetoO, double hipotenusa)
+        {
+            if(catetoO <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return catetoO / hipotenusa;
+            }
+        }// fim do metodo 
+
+        //20.
+        public double CalcularCosseno(double adjacente, double hipo)
+        {
+            if (adjacente <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return adjacente / hipo;
+            }
+        }//fim do método
+
+        //21
+        public double AreaTrapezio(double baseM,double baseP,double alt)
+        {
+            if (baseM <= 0)
+            {
+                return -1;
+            }
+            return (baseM + baseP) * alt / 2;
+        }// fim do metodo
+
+        //22.
+        public double ConverterDias(double dias)
+        {
+            if(dias <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return dias * 86400;
+            }
+        }//fim do metodo
+         //23
+        public int SomaDigitos(int numero)
+        {
+            int soma = 0;
+            while (numero > 0)
+            {
+                soma += numero % 10; // pega o último dígito e soma
+                numero /= 10;        // remove o último dígito do número
+            }
+            return soma;
+        }
+        /*/24
+       // public double InversoMultiplicativo(double inverso)
+        {
+
+        }// fim do metodo */
+
+        //25.
+        public double CalcularHipotenusa(double cate1, double cate2)
+        {
+            return Math.Sqrt( (cate1 * cate1) + (cate2 * cate2));
+        }
         
+        //26
+        public string ConverterBinario(int num)
+        {
+
+            string binario = "";// aspas significa que começa vazio
+
+            while (num != 0)
+            {
+                binario = "" + num % 2; //( = recebe (<)) (% = mod /)
+                num /= 2;
+            }//fim do while
+
+            { return binario; }
+
+        }// fim do método
+        public string Inverter(string valor)
+        {
+            int tamanho = valor.Length;
+            string binarioInvertido = "";
+            for (int i = tamanho - 1; i >= 0; i--)
+            {
+                {
+                    binarioInvertido += valor.Substring(i, 1);
+                }
+
+            }//fim do for 
+            return binarioInvertido;
+        }//fim do metodo 
+
+        //27.Leia um numero e exiba seu correspondente em hexadecimal
+        public string Hexadecimal(int decim)
+        {
+            string hexa = "";
+            string result = "";
+            while (decim != 0)
+            {
+                result = "" + decim % 16;
+                switch (result)
+                {
+                    case "10":
+                        result = "A";
+                        break;
+                    case "11":
+                        result = "B";
+                        break;
+                    case "12":
+                        result = "C";
+                        break;
+                    case "13":
+                        result = "D";
+                        break;
+                    case "14":
+                        result = "E";
+                        break;
+                    case "15":
+                        result = "F";
+                        break;
+                }//fim do switch
+                decim /= 16;
+                hexa += result;
+            }//fim do while
+            return Inverter(hexa);
+
+        }//fim do método
+
+        //29.
+        public double CalcularJuros(double valor)
+        {
+            if ( valor <= 0 )
+            {
+                return -1;
+            }
+            else
+            {
+                return valor + (valor * 0.15);
+            }
+        }// fim do metodo
+
+        //30.
+        public double CalcularComposto(double composto)
+        {
+            if ( composto <= 0 )
+            {
+                return -1;
+            }
+            else
+            {
+                return composto / 100;
+            }
+        }//fim do metodo
+
+        //31
+       /* public double ConverterGraus(double graus)
+        {
+
+        }// fim do metodo
+       */
 
 
-    }//fim da classe 
+
+        //32.
+        public double CalcularPorcentagem(double porcento)
+        {
+            return porcento / 100;
+        }// fim do metodo 
+
+
+
+
+        
+    }//fim da classe
 }//fim do projeto
